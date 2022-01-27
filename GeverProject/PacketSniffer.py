513d4af032189cf7e9fdda92cb2b425d6b64a4ba
GuyGever <94006611+GuyGever@users.noreply.github.com>
2022-01-27 16:37:44 -0500

    Delete PacketSniffer.py

--- GeverProject/PacketSniffer.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-import time
-i = int(input("Enter How Much Time Do You Want To Sniff For? "))
-a = AsyncSniffer(iface=["eth0","wlan0"])
-a.start()
-time.sleep(i)
-a.stop()
-print("Captured {0} Packets".format(len(a.results)))
-print(a.results)
